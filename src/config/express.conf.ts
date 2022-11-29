a32c172ecca28de7b63e08415350471486072a03
Bitlatte <me@cobymckinney.com>
2022-11-29 17:46:43 +0000

    chore: add helmet, cors, and morgan

--- src/config/express.conf.ts
+++ src/config/express.conf.ts
@@ -1,7 +1,18 @@
-import express, { Application } from "express";
+import express, { Application, urlencoded, json } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 import AppRouter from "../routes";
 
 const App: Application = express();
+
+App.disable("x-powered-by");
+
+App.use(urlencoded({ extended: false }));
+App.use(json());
+App.use(cors());
+App.use(helmet());
+App.use(morgan("dev"));
 
 App.use("/", AppRouter);
 
